5b2cc711fd28630b8fb137bb812c2ebbe7690291
whquddn <whquddn55@gmail.com>
2017-04-03 21:53:08 +0900

    10869.cpp Solved

---
+++ 10000/10869.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin >> a >> b;
+	cout << (a + b) << '\n';
+	cout << (a - b) << '\n';
+	cout << (a * b) << '\n';
+	cout << (a / b) << '\n';
+	cout << (a % b) << '\n';
+	return 0;
+}
