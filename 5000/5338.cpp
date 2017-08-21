d6c8e4bd0aeb7f5a52adc77421dfdac64ec0ae29
whquddn <whquddn55@gmail.com>
2017-08-21 12:02:32 +0900

    5338.cpp Solved

---
+++ 5000/5338.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MAXSIZE 100
+
+int main() {
+	cout << "       _.-;;-._" << endl;
+	cout << "\'-..-\'|   ||   |" << endl;
+	cout << "\'-..-\'|_.-;;-._|" << endl;
+	cout << "\'-..-\'|   ||   |" << endl;
+	cout << "\'-..-\'|_.-\'\'-._|" << endl;
+
+
+	return 0;
+}
