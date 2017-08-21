e2309cfb188a13e68700ad90e5fa83ef51d70ada
whquddn <whquddn55@gmail.com>
2017-08-21 18:46:06 +0900

    10951.cpp Solved

---
+++ 10000/10951.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MAXSIZE 125
+#define INF 987654321
+
+int main() {
+	int a, b;
+	while(scanf("%d %d", &a, &b) != EOF)
+		cout << a + b << endl;
+
+
+	return 0;
+}
+
