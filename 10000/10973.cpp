17e75a166bb264af6c34d2d9da87eb6c0d48356e
whquddn <whquddn55@gmail.com>
2017-09-03 14:28:24 +0900

    10973.cpp Solved

---
+++ 10000/10973.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+#define INF 987654321
+#define MAXSIZE 1000
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    bool flag = false;
+    for(int i = n - 2; i >= 0; i--){
+        int max = -INF;
+        int idx;
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] < arr[j]){
+                continue;
+            }
+
+            if( max < arr[j]){
+                max = arr[j];
+                idx = j;
+            }
+            flag = true;
+        }
+        if(!flag){
+            continue;
+        }
+        int tmp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = tmp;
+
+        sort(arr.begin() + i + 1, arr.end());
+        reverse(arr.begin() + i + 1, arr.end());
+        break;
+    }
+
+    if(!flag) {
+        printf("-1\n");
+    }
+
+    else {
+        for(int i = 0; i < n; i++){
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+
+    
+
+	return 0;
+}
+
