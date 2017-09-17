f945cf1f26aff64c85eca589735bb49d6c3d8c4f
whquddn <whquddn55@gmail.com>
2017-09-17 23:00:52 +0900

    3053.cpp Solved

---
+++ 3000/3053.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#define INF 987654321
+using namespace std;
+
+int main() {
+    int r;
+    scanf("%d", &r);
+
+    printf("%.6lf\n%.6lf\n", r * r * M_PI, r * r * 2.0);
+
+    return 0;
+}
