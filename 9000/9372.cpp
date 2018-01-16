286e9117110165bd951acc6dc4f056dea3bc9cfa
whquddn <whquddn55@gmail.com>
2018-01-16 23:40:15 +0900

    9372.cpp Solved

---
+++ 9000/9372.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int tc;
+	scanf("%d", &tc);
+	
+	while (tc--) {
+		int n, m;
+		scanf("%d %d", &n, &m);
+		for (int i = 0; i < m; i++) {
+			int temp;
+			scanf("%d %d", &temp, &temp);
+		} 
+		
+		printf("%d\n", n - 1);
+		
+	}
+}
