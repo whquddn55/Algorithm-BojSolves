49b3544bfaaf4959db3f3cdf0567a8fb19e696c2
whquddn <whquddn55@gmail.com>
2018-02-04 10:32:08 +0900

    3745.cpp Solved

---
+++ 3000/3745.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF (INT_MAX / 2)
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int n;
+vector<int> arr;
+vector<int> lis;
+
+int main() {
+	while (scanf("%d", &n) != EOF) {
+		arr.clear();
+		lis.clear();
+		arr.resize(n);
+		for (int i = 0; i < n; i++)
+			scanf("%d", &arr[i]);
+
+		lis.push_back(arr[0]);
+		for (int i = 1; i < n; i++) {
+			if (lis[lis.size() - 1] < arr[i])
+				lis.push_back(arr[i]);
+			else
+				lis[lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin()] = arr[i];
+		}
+
+		printf("%d\n", lis.size());
+	}
+	
+	return 0;
+}
