0e7ecc2fcf75eebba29846f3a3d7d42b958d9b10
whquddn <whquddn55@gmail.com>
2018-02-15 13:23:16 +0900

    10992.cpp Solved

---
+++ 10000/10992.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF (INT_MAX / 2)
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int n;
+
+int main() {
+	scanf("%d", &n);
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < (n - i); j++)
+			printf(" ");
+		printf("*");
+		for (int j = 0; j < (i - 2) * 2 + 1; j++)
+			printf(" ");
+		if (i != 1)
+			printf("*");
+		printf("\n");
+	}
+	for (int i = 0; i < n * 2 - 1; i++)
+		printf("*");
+	printf("\n");
+
+	return 0;
+}
