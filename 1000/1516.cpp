0fa75a79280b604688c0993f8fb6e82f8afeeb1e
whquddn <whquddn55@gmail.com>
2018-02-28 16:15:25 +0900

    1516.cpp Solved

---
+++ 1000/1516.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using std::pair;
+using std::vector;
+using std::queue;
+using std::stack;
+using std::priority_queue;
+using std::set;
+using std::map;
+using std::deque;
+using std::greater;
+using std::max;
+using std::min;
+using std::sort;
+using std::binary_search;
+using std::lower_bound;
+using std::upper_bound;
+using std::reverse;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define INF (INT_MAX / 2)
+
+int n, k;
+vector<vector<int>> edges;
+vector<int> income;
+vector<int> cost;
+
+void topologicalSort() {
+	priority_queue<int, vector<int>, greater<int>> q;
+	vector<int> result(n + 1, 0);
+
+	for (int i = 1; i <= n; i++) 
+		if (income[i] == 0) {
+			q.push(i);
+			result[i] = cost[i];
+		}
+
+	for (int i = 0; i < n; i++) {
+		if (q.empty()) {
+			printf("-123\n");
+			return;
+		}
+
+		int here = q.top();
+		q.pop();
+
+		for (int j = 0; j < edges[here].size(); j++) {
+			int next = edges[here][j];
+			income[next]--;
+			result[next] = max(result[next], cost[next] + result[here]);
+			if (income[next] == 0) 
+				q.push(next);
+			
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+		printf("%d\n", result[i]);
+}
+
+int main() {
+	scanf("%d", &n);
+	edges.resize(n + 1);
+	income.resize(n + 1, 0);
+	cost.resize(n + 1);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &cost[i]);
+
+		while (true) {
+			int from;
+			scanf("%d", &from);
+			if (from == -1)
+				break;
+			edges[from].push_back(i);
+			income[i]++;
+		}
+	}
+
+	topologicalSort();
+
+	return 0;
+
+}
