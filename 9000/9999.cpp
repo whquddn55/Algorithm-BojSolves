955f35ed7a2a448e627edebeb93f1b69c4622c34
whquddn <whquddn55@gmail.com>
2018-03-21 22:13:40 +0900

    9999.cpp Solved

---
+++ 9000/9999.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define INF (INT_MAX / 2)
+#define endl '\n'
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	cout << "Hymn To Love" << endl;
+	
+	return 0;
+}
