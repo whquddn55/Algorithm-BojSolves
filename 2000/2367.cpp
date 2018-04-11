a0e69f6c18150f135ec64884fb5a55fe6bdb6ab3
whquddn <whquddn55@gmail.com>
2018-04-11 14:36:40 +0900

    2367.cpp Solved

---
+++ 2000/2367.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define INF (INT_MAX / 2)
+#define endl '\n'
+
+#define MAX_SIZE 101
+#define MAX_V 303
+
+struct Edge {
+	int to, capacity, flow, rev;
+
+	Edge(int to, int capacity, int rev) 
+		: to(to), capacity(capacity), rev(rev), flow(0) { }
+};
+
+int n, k, d;
+
+int level[MAX_V];
+int iter[MAX_V];
+vector<Edge> graph[MAX_V];
+
+void bfs(int start) {
+	memset(level, -1, sizeof(level));
+	queue<int> q;
+	q.push(start);
+	level[start] = 0;
+
+	while (!q.empty()) {
+		int here = q.front();
+		q.pop();
+
+		for (Edge& e : graph[here]) {
+			if ((e.capacity - e.flow) > 0 && level[e.to] < 0) {
+				level[e.to] = level[here] + 1;
+				q.push(e.to);
+			}
+		}
+	}
+}
+
+int dfs(int here, int to, int flow) {
+	if (here == to)
+		return flow;
+	for (int& i = iter[here]; i < graph[here].size(); i++) {
+		Edge& e = graph[here][i];
+		if ((e.capacity - e.flow) > 0 && level[here] < level[e.to]) {
+			int d = dfs(e.to, to, min(flow, (e.capacity - e.flow)));
+			if (d > 0) {
+				e.flow += d;
+				graph[e.to][e.rev].flow -= d;
+				return d;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int maxFlow(int here, int to) {
+	int flow = 0;
+	while (true) {
+		bfs(here);
+		if (level[to] < 0)
+			return flow;
+		memset(iter, 0, sizeof(iter));
+		int f;
+		while ((f = dfs(here, to, INF)) > 0)
+			flow += f;
+	}
+	return flow;
+}
+
+void addEdge(int from, int to, int capacity) {
+	graph[from].emplace_back(to, capacity, graph[to].size());
+	graph[to].emplace_back(from, 0, graph[from].size() - 1);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	
+	cin >> n >> k >> d;
+	int source = 0;
+	int leftStart = 1;
+	int rightStart = leftStart + n;
+	int sink = rightStart + d;
+
+	for (int left = 0; left < n; left++)
+		addEdge(source, leftStart + left, k);
+
+	for (int right = 0; right < d; right++) {
+		int a;
+		cin >> a;
+		addEdge(rightStart + right, sink, a);
+	}
+	for (int left = 0; left < n; left++) {
+		int z;
+		cin >> z;
+		while (z--) {
+			int a;
+			cin >> a;
+			addEdge(leftStart + left, rightStart + a - 1, 1);
+		}
+	}
+
+	cout << maxFlow(source, sink) << endl;
+
+	return 0;
+}
