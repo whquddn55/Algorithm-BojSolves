48dc06a145bc7f1e5f58d0e76aee848d7da8aa2c
whquddn <whquddn55@gmail.com>
2018-05-13 00:41:48 +0900

    9249.cpp Solved

---
+++ 9000/9249.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define INF (INT_MAX / 2)
+#define endl '\n'
+
+#define MAX_N 300001
+
+string a, b;
+string str;
+int t;
+int sA[MAX_N]; // suffix array
+int g[MAX_N]; // 현재 비교할 group
+int tg[MAX_N]; // 다음에 비교할 group
+int sAReverse[MAX_N]; // suffix array의 inverse
+int lcp[MAX_N]; // lcp
+
+int ans = 0;
+string ansStr;
+
+bool cmp(int x, int y) {
+	if (g[x] == g[y])
+		return g[x + t] < g[y + t];
+	return g[x] < g[y];
+}
+
+void getSA() {
+	t = 1;
+	for (int i = 0; i < str.size(); i++) {
+		sA[i] = i;
+		g[i] = str[i] - 'a';
+	}
+
+	while (t <= str.size()) {
+		g[str.size()] = -1;
+		sort(sA, sA + str.size(), cmp);
+
+		tg[sA[0]] = 0;
+		for (int i = 1; i < str.size(); i++) {
+			if (cmp(sA[i - 1], sA[i]))
+				tg[sA[i]] = tg[sA[i - 1]] + 1;
+			else
+				tg[sA[i]] = tg[sA[i - 1]];
+		}
+
+		for (int i = 0; i < str.size(); i++)
+			g[i] = tg[i];
+		t *= 2;
+	}
+}
+
+void getLcp() {
+	for (int i = 0; i < str.size(); i++)
+		sAReverse[sA[i]] = i;
+	int len = 0;
+	for (int i = 0; i < str.size(); i++) {
+		int k = sAReverse[i];
+		if (k) {
+			int j = sA[k - 1];
+			while (str[j + len] == str[i + len] && ((j + len < a.size() && i + len > a.size()) || (j + len > a.size() && i + len < a.size())))
+				len++;
+			lcp[k] = len;
+			if (ans < lcp[k]) {
+				ans = lcp[k];
+				ansStr.clear();
+				for (int i = 0; i < len; i++)
+					ansStr.push_back(str[j + i]);
+			}
+			if (len)
+				len--;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	cin >> a >> b;
+	str.append(a);
+	str.push_back('!');
+	str.append(b);
+	getSA();
+	getLcp();
+	
+	cout << ans << endl;
+	cout << ansStr << endl;
+
+	return 0;
+}
