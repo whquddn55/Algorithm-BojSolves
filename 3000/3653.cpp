8213abd58e9927767807554a6c5927770f7fa93d
whquddn <whquddn55@gmail.com>
2018-07-30 19:40:42 +0900

    3653.cpp Solved

---
+++ 3000/3653.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+ostream& operator<<(ostream& os, const pii& pai) {
+	os << pai.first << ' ' << pai.second;
+	return os;
+}
+
+istream& operator>>(istream& is, pii& pai) {
+	is >> pai.first >> pai.second;
+	return is;
+}
+
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+
+#define INF (INT_MAX / 2)
+
+#define MAX_N 200000
+
+int n, m;
+int pos[MAX_N];
+int tree[MAX_N * 4];
+
+int update(int index, int value, int id = 1, int nodeLeft = 0, int nodeRight = MAX_N - 1) {
+	int mid = (nodeLeft + nodeRight) / 2;
+	// 범위를 완전히 벗어나면 아무 것도 안 한다.
+	if (index < nodeLeft || index > nodeRight)
+		return tree[id];
+	// index에 해당하는 leafNode에 도착하면 값 변경
+	if (nodeLeft == nodeRight)
+		return tree[id] = value;
+	// 그 외의 경우 분할정복
+	return tree[id] = update(index, value, id * 2, nodeLeft, mid) + update(index, value, id * 2 + 1, mid + 1, nodeRight);
+}
+
+int query(int left, int right, int id = 1, int nodeLeft = 0, int nodeRight = MAX_N - 1) {
+	int mid = (nodeLeft + nodeRight) / 2;
+	// 범위를 완전히 벗어나면 아무 것도 안 한다.
+	if (right < nodeLeft || left > nodeRight)
+		return 0;
+	// 범위에 완전히 포함되는 경우 tree의 값이 부분합의 일부분이 된다.
+	if (left <= nodeLeft && right >= nodeRight)
+		return tree[id];
+	// 그 외의 경우 분할정복
+	return query(left, right, id * 2, nodeLeft, mid) + query(left, right, id * 2 + 1, mid + 1, nodeRight);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		memset(tree, 0, sizeof(tree));
+		cin >> n >> m;
+		for (int i = 0; i < n; i++) {
+			pos[i] = n - i - 1;
+			update(i, 1);
+		}
+
+		int height = n - 1;
+		for (int i = 0; i < m; i++) {
+			int a;
+			cin >> a;
+			a--;
+			cout << query(pos[a] + 1, height) << ' ';
+			update(pos[a], 0);
+			height++;
+			pos[a] = height;
+			update(pos[a], 1);
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
