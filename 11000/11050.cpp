b8a791d45e607b7174251da1f681d8c50ceac3e9
whquddn <whquddn55@gmail.com>
2018-08-19 20:59:02 +0900

    11050.cpp Solved

---
+++ 11000/11050.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename a, typename b>
+ostream& operator<<(ostream& os, const pair<a, b>& pai) {
+	os << pai.first << ' ' << pai.second;
+	return os;
+}
+template<typename a, typename b>
+istream& operator>>(istream& is, pair<a, b>& pai) {
+	is >> pai.first >> pai.second;
+	return is;
+}
+
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+
+#define INF (INT_MAX / 2)
+
+#define MAX_N 11
+
+int n, k;
+ll dp[MAX_N][MAX_N];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	cin >> n >> k;
+	for (int i = 0; i <= n; i++)
+		dp[i][0] = 1;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= k; j++)
+			dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+
+	cout << dp[n][k] << endl;
+	
+	return 0;
+}
