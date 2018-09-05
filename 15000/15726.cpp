1dfea2fdb582e21d141aa2e06071ea18dd962c95
whquddn <whquddn55@gmail.com>
2018-09-05 16:28:51 +0900

    15726.cpp Solved

---
+++ 15000/15726.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename a, typename b>
+ostream& operator<<(ostream& os, const pair<a, b>& pai) {
+	os << pai.first << ' ' << pai.second;
+	return os;
+}
+template<typename a, typename b>
+istream& operator>>(istream& is, pair<a, b>& pai) {
+	is >> pai.first >> pai.second;
+	return is;
+}
+
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+
+#define INF (INT_MAX / 2)
+
+#define MAX_N 100
+
+ll a, b, c;
+
+int main() {
+	ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+
+	cin >> a >> b >> c;
+	cout << (int)max(a * b / c, a * c / b) << endl;
+	
+	return 0;
+}
