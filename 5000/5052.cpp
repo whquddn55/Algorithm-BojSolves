9ad3bbadec8f552c5e3153581ff26d6916cc2ecf
whquddn <whquddn55@gmail.com>
2021-02-24 21:01:11 +0900

    5052.cpp Solved

---
+++ 5000/5052.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename a, typename b>
+ostream& operator<<(ostream& os, const pair<a, b>& pai) {
+	os << pai.first << ' ' << pai.second;
+	return os;
+}
+template<typename a, typename b>
+istream& operator>>(istream& is, pair<a, b>& pai) {
+	is >> pai.first >> pai.second;
+	return is;
+}
+
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+
+#define INF (INT_MAX / 2)
+
+#define MAX_N 10005
+#define MOD 1000000007
+
+int n;
+string arr[MAX_N];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		cin >> n;
+		for (int i = 0; i < n; ++i)
+			cin >> arr[i];
+		sort(arr, arr + n);
+
+		bool result = false;
+		string value = arr[0];
+		for (int i = 1; i < n; i++) {
+			if (arr[i].substr(0, value.size()) == value) {
+				result = true;
+				break;
+			}
+			else
+				value = arr[i];
+			
+		}
+		cout << (result ? "NO" : "YES") << endl;
+	}
+	
+
+	return 0;
+}
