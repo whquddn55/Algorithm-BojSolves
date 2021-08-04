65bf47d57a22c77882eff55d0d1b4682fb96668e
whquddn <whquddn55@gmail.com>
2021-08-04 17:24:05 +0900

    13711.cpp Solved

---
+++ 13000/13711.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename a, typename b>
+ostream& operator<<(ostream& os, const pair<a, b>& pai) {
+    os << pai.first << ' ' << pai.second;
+    return os;
+}
+template<typename a, typename b>
+istream& operator>>(istream& is, pair<a, b>& pai) {
+    is >> pai.first >> pai.second;
+    return is;
+}
+
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+
+#define INF (INT_MAX / 2)
+#define MAX_N 100005
+#define MOD 1000000007
+
+int n;
+int arr[MAX_N];
+int idx[MAX_N];
+vector<int> lis;
+
+int main() {
+	ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+	for (int i = 0; i < n; ++i) {
+		int t;
+		cin >> t;
+		idx[t] = i;
+	}
+
+	lis.emplace_back(idx[arr[0]]);
+	for (int i = 1; i < n; ++i) {
+		auto it = lower_bound(lis.begin(), lis.end(), idx[arr[i]]);
+		if (it == lis.end())
+			lis.emplace_back(idx[arr[i]]);
+		else
+			*it = idx[arr[i]];
+	}
+
+	cout << lis.size() << endl;
+	
+	return 0;
+}
