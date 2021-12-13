a7b66c1a2fdb562555a1fe62264799257492671f
조병우 <whquddn55@gmail.com>
2021-12-14 06:01:09 +0900

    13322.cpp Solved

---
+++ 13000/13322.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#pragma GCC target("avx2")
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename a, typename b>
+ostream& operator<<(ostream& os, const pair<a, b>& pai) {
+    os << pai.first << ' ' << pai.second;
+    return os;
+}
+template<typename a, typename b>
+istream& operator>>(istream& is, pair<a, b>& pai) {
+    is >> pai.first >> pai.second;
+    return is;
+}
+
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+
+#define all(x) x.begin(), x.end()
+#define INF (INT_MAX / 2)
+#define MAX_N 2005
+#define MOD 1'000'000'007
+
+string s;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> s;
+    for (int i = 0; i < s.size(); ++i)
+        cout << i << endl;
+    
+    return 0;
+}
