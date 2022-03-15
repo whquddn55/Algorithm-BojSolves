3604564ca2d279497835fa944e72b5df8f80af18
thuthi <whquddn55@gmail.com>
2022-03-15 20:14:31 +0900

    23890.cpp Solved

---
+++ 23000/23890.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#pragma GCC target("avx2")
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+template<typename a, typename b>
+ostream& operator<<(ostream& os, const pair<a, b>& pai) {
+    os << pai.first << ' ' << pai.second;
+    return os;
+}
+template<typename a, typename b>
+istream& operator>>(istream& is, pair<a, b>& pai) {
+    is >> pai.first >> pai.second;
+    return is;
+}
+
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+
+#define all(x) x.begin(), x.end()
+#define INF (INT_MAX / 2)
+#define MAX_N 1000005
+#define MOD 1'000'000'007
+#define MOD2 1'000'000'009
+
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int r;
+    cin >> r;
+    ll x = 1;
+    while (x * x < 2 * r - 1)
+        ++x;
+    --x;
+    cout << pii(x, r - 1) << endl;
+    
+    return 0;
+}
